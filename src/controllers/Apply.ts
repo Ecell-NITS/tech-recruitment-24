8fc957269bdc16692b18578174880d6569dc3a03
ruler45 <sahin0945@gmail.com>
2024-06-19 15:48:36 +0530

    Multiple team application

--- src/controllers/Apply.ts
+++ src/controllers/Apply.ts
@@ -8,18 +8,22 @@ export const getApplications = async (_req: Response, res: Request) => {
 };
 
 export const createApplication = async (req: Request, res: Response) => {
-  const { name, email, scholarId, resumeUrl, WhyEcell, number, domain } =
+  const { name, email, scholarId, resumeUrl, WhyEcell, number, teams } =
     req.body;
   if (
-    domain !== "Event" &&
-    domain !== "Content" &&
-    domain !== "Curation" &&
-    domain !== "Design" &&
-    domain !== "Marketing" &&
-    domain !== "Publicity" &&
-    domain !== "Collaboration"
+    !name ||
+    !email ||
+    !scholarId ||
+    !resumeUrl ||
+    !WhyEcell ||
+    !number ||
+    !teams
   ) {
-    res.status(400).json({ message: "Invalid Team" });
+    res.status(400).json({ message: "Please fill all the fields" });
+    return;
+  }
+  if (teams.length === 0) {
+    res.status(400).json({ message: "Please select atleast one team" });
     return;
   }
 
@@ -29,46 +33,39 @@ export const createApplication = async (req: Request, res: Response) => {
     return;
   }
 
-  let team = {
-    teamName: "",
-    teamLink: "",
-  };
-  if (domain === "Event") {
-    team = {
-      teamName: "Event Management",
-      teamLink: "https://discord.gg/2Y9z8e8J",
-    };
-  } else if (domain === "Content") {
-    team = {
-      teamName: "Content",
-      teamLink: "https://discord.gg/2Y9z8e8J",
-    };
-  } else if (domain === "Curation") {
-    team = {
-      teamName: "Curation",
-      teamLink: "https://discord.gg/2Y9z8e8J",
-    };
-  } else if (domain === "Design") {
-    team = {
-      teamName: "Design",
-      teamLink: "https://discord.gg/2Y9z8e8J",
-    };
-  } else if (domain === "Marketing") {
-    team = {
+  const availableTeams = [
+    {
       teamName: "Marketing",
-      teamLink: "https://discord.gg/2Y9z8e8J",
-    };
-  } else if (domain === "Publicity") {
-    team = {
+      teamLink: "https://chat.whatsapp.com/FEQssVGXlHE9mCuHV20Koo",
+    },
+    {
+      teamName: "Content",
+      teamLink: "https://chat.whatsapp.com/GlWVKkWotKu6L750tQoJak",
+    },
+    {
+      teamName: "Design",
+      teamLink: "https://chat.whatsapp.com/BvLSzQjyqAF7A5WmwvNdo8",
+    },
+    {
       teamName: "Publicity",
-      teamLink: "https://discord.gg/2Y9z8e8J",
-    };
-  } else if (domain === "Collaboration") {
-    team = {
-      teamName: "Collaboration & Outreach",
-      teamLink: "https://discord.gg/2Y9z8e8J",
-    };
-  }
+      teamLink: "https://chat.whatsapp.com/KGdzzQOZBilHHEyveN1pds",
+    },
+    {
+      teamName:"Event",
+      teamLink:"https://chat.whatsapp.com/BlUmN5ywJzrBRPKzA12heu"
+    },
+    {
+      teamName:"Collaboration",
+      teamLink:"https://chat.whatsapp.com/F2NJ25pjp7RAJAeVGhY3Y9"
+    },
+    {
+      teamName:"Curation",
+      teamLink:"https://chat.whatsapp.com/KRlRL4D2K88EzgL5cGIQdc"
+    }
+  ];
+  const yourTeams = availableTeams.filter((team) =>
+    teams.includes(team.teamName)
+  );
 
   try {
     prisma.application
@@ -79,16 +76,19 @@ export const createApplication = async (req: Request, res: Response) => {
           scholarId,
           resumeUrl,
           number,
-          domain,
+          teams,
           WhyEcell,
         },
       })
       .then((data) => {
         const sunject = "Application for joining E-cell NIT Silchar";
-        const text = `Thank you for applying to the ${team.teamName} team of E-cell NIT Silchar. We will get back to you soon.In the meanwhile, you can join our WhatsApp group  ${team.teamLink}`;
+        const text = `Thank you for applying to the exciting teams of E-cell NIT Silchar. We will get back to you soon.In the meanwhile, you should join our WhatsApp group of the teams you have applied for.
+        ${yourTeams.map((team) => `<a href="${team.teamLink}"><strong>${team.teamName}</strong></a>`)}
+        `;
         const html = `
-        <h2>Thank you for applying to the ${team.teamName} team of E-cell NIT Silchar.</h2>
-        <p>We will get back to you soon.In the meanwhile, you can join our WhatsApp group <strong> <a href="${team.teamLink}">${team.teamName}</a></strong></p>`;
+        <h2>Thank you for applying to the exciting teams of E-cell NIT Silchar.</h2>
+        <p>We will get back to you soon.In the meanwhile, you should join our WhatsApp group of the teams you have applied for.
+        ${yourTeams.map((team) => `<a href="${team.teamLink}"><strong>${team.teamName}</strong></a> <br>`)}`;
         sendEmail(email, sunject, text, html);
         res.json(data).status(200);
       })
@@ -101,28 +101,21 @@ export const createApplication = async (req: Request, res: Response) => {
   }
 };
 export const existApplication = async (req: Request, _res: Response) => {
-  const { email, number, scholarId, resumeUrl, domain } = req.body;
+  const { email, number, scholarId, resumeUrl} = req.body;
   const Application = await prisma.application.findFirst({
     where: {
-      AND: [
+      OR: [
         {
-          domain: domain,
+          email: email,
         },
         {
-          OR: [
-            {
-              email: email,
-            },
-            {
-              number: number,
-            },
-            {
-              scholarId: scholarId,
-            },
-            {
-              resumeUrl: resumeUrl,
-            },
-          ],
+          number: number,
+        },
+        {
+          scholarId: scholarId,
+        },
+        {
+          resumeUrl: resumeUrl,
         },
       ],
     },
@@ -140,4 +133,3 @@ export const getMyApplication = async (req: Request, res: Response) => {
   });
   res.json(myApplications);
 };
-
